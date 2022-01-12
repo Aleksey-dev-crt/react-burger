a14fb6a8b7d4e4967c209e109e87c1e7b33007bf
Aleksey <1862647@gmail.com>
2022-01-12 15:45:46 +0700

    edit gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import AppHeader from './components/AppHeader/AppHeader';
 import BurgerIngredients from './components/BurgerIngredients/BurgerIngredients';
 import BurgerConstructor from './components/BurgerConstructor/BurgerConstructor';
